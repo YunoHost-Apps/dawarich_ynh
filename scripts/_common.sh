24f024315a6353509610e81fb540e014d1489140
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-07-31 21:28:37 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,21 @@
 #=================================================
 
 ruby_version="3.3.5"
+
+case $YNH_ARCH in
+	amd64)
+		ld_preload="LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so"
+		;;
+	arm64)
+		ld_preload="LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so"
+		;;
+	armhf)
+		ld_preload="LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libjemalloc.so"
+		;;
+	armel)
+		ld_preload="LD_PRELOAD=/usr/lib/arm-linux-gnueabi/libjemalloc.so"
+		;;
+	i386)
+		ld_preload="LD_PRELOAD=/usr/lib/i386-linux-gnu/libjemalloc.so"
+		;;
+esac
